926e00ca8a5c4c7c28275ebfe765a7282bf1e477
cyborg24 <53658824+cyborg24@users.noreply.github.com>
2022-07-01 14:45:37 +0100

    Update usdt.py

--- usdt.py
+++ usdt.py
@@ -44,9 +44,9 @@ if __name__ == '__main__':
     api_key1 = "38ZEGS8DSA6ADH48HWHEEPRNBQ5WTMV79A"
     api_key2 = "XYRXQIKKZHYJA6BYYY9BJKPASW2464FUFQ"
     api_key3 = "3KSDKHDR7APAF3I2NF1BSQPD834M8D7TXC"
-    p1 = mp.Process(target=mainProg, args=(api_key1, 1,3999,))
-    p2 = mp.Process(target=mainProg, args=(api_key2, 2,4000,))
-    p3 = mp.Process(target=mainProg, args=(api_key3, 3,4001,))
+    p1 = mp.Process(target=mainProg, args=(api_key1, 1,4001,))
+    p2 = mp.Process(target=mainProg, args=(api_key2, 2,4002,))
+    p3 = mp.Process(target=mainProg, args=(api_key3, 3,4003,))
     """
     p4 = mp.Process(target=mainProg)
     p5 = mp.Process(target=mainProg)
